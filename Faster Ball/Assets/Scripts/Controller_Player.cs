2fbe72af06fa5b08b0e2d5f93b3057b4e10b2499
Thomas <tomasz.klos1@gmail.com>
2020-01-05 00:14:05 +0100

    #1 - 5/01/20
    Move Player.

---
+++ Faster Ball/Assets/Scripts/Controller_Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Player : MonoBehaviour
+{
+    //Variables
+    public float moveSpeed = 25;
+    public GameObject player;
+
+    private Rigidbody2D playerBody;
+    private float screenWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenWidth = Screen.width;
+        playerBody = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int i = 0;
+        while (i < Input.touchCount)
+        {
+            if (Input.GetTouch(i).position.x > screenWidth / 2)
+                RunPlayer(1.0f);
+
+            if (Input.GetTouch(i).position.x < screenWidth / 2)
+                RunPlayer(-1.0f);
+
+            ++i;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        #if UNITY_EDITOR
+        RunPlayer(Input.GetAxis("Horizontal"));
+        #endif
+    }
+
+    //The fucking move player
+    private void RunPlayer(float horizontalPlayer)
+    {
+        playerBody.AddForce(new Vector2(horizontalPlayer * moveSpeed * Time.deltaTime, 0));
+    }
+}
